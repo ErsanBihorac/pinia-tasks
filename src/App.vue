eddc52e8c9e95bc0ff5bd340a12619e3fea57b54
Ersan Bihorac <ersan.bihorac@milhouse.solutions>
2025-01-08 10:02:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+
+    <!-- header -->
+    <header>
+      <img src="./assets/pinia-logo.png" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
+    <!-- filter nav -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <!-- task list -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+  </main>
+</template>
+
+<script>
+import { ref } from 'vue';
+import TaskDetails from './components/TaskDetails.vue';
+import TaskForm from './components/TaskForm.vue';
+import { useTaskStore } from './stores/TaskStore';
+
+export default {
+  components: { TaskDetails, TaskForm },
+  setup() {
+    const taskStore = useTaskStore()
+
+    const filter = ref("all")
+
+    return { taskStore, filter }
+  }
+}
+</script>
